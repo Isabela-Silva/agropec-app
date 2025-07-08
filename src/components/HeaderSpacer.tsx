554b18d311b689f19fb63d2d9144062e12a21341
jovemcleb <lima.cardoso.caleb@gmail.com>
2025-07-08 17:07:07 -0300

    chore: remove arquivos desnecessários

--- src/components/HeaderSpacer.tsx
+++
@@ -1,4 +0,0 @@
-// Componente para adicionar espaçamento quando o header está fixo
-export function HeaderSpacer() {
-  return <div className="h-[57px]" />; // Altura aproximada do header (py-3 + border)
-}
